85373bad0f3d0e461abbea00a019445424c5d543
blipblipblipDEV <66701157+blipblipblipDEV@users.noreply.github.com>
2020-10-10 18:51:15 -0400

    Added description, discord, group, and application buttons.

--- The Breakthrough Studios App/The Breakthrough Studios App/Form1.Designer.cs
+++ The Breakthrough Studios App/The Breakthrough Studios App/Form1.Designer.cs
@@ -31,6 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,22 +42,64 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Raleway SemiBold", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(136, 181);
+            this.label1.Location = new System.Drawing.Point(203, 191);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(535, 53);
+            this.label1.Size = new System.Drawing.Size(416, 53);
             this.label1.TabIndex = 0;
-            this.label1.Text = "About Breakthrough Studios";
+            this.label1.Text = "Breakthrough Studios";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(315, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(298, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(187, 176);
+            this.pictureBox1.Size = new System.Drawing.Size(214, 195);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(352, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "The best studio on ROBLOX, and the best games, too!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Raleway SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(231, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Join Discord";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Raleway SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(414, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Join Group";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Raleway SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(160, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(476, 58);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Apply for staff (Requires group join)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -61,6 +107,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -77,6 +127,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- The Breakthrough Studios App/The Breakthrough Studios App/Form1.cs
+++ The Breakthrough Studios App/The Breakthrough Studios App/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,21 @@ namespace The_Breakthrough_Studios_App
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/Y3X5g4u");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.roblox.com/groups/6590206/Break-through-studios#!/about");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.roblox.com/games/5426365206/Applications");
         }
     }
 }
